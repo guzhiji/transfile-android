8f6e9771965b880056ae244dee4d0b14c1a599ed
Zhiji Gu <zhiji_gu@feiliks.com>
2018-09-27 16:08:04 +0800

    broadcast on all IPv4 interfaces

--- app/src/main/java/guzhijistudio/transfile/utils/Constants.java
+++ app/src/main/java/guzhijistudio/transfile/utils/Constants.java
@@ -1,8 +1,9 @@
 package guzhijistudio.transfile.utils;
 
 public final class Constants {
-    public final static int FILE_SERVER_PORT = 8889;
-    public final static int IDENTITY_SERVER_PORT = 8888;
+    public final static int FILE_SERVER_PORT = 18889;
+    public final static int IDENTITY_SERVER_PORT = 18888;
+    public final static int IDENTITY_LOCAL_PORT = 18887;
     public final static String IDENTITY_GROUP_ADDR = "224.0.0.255";
     public final static String ACTION_FILE_SENDER = "guzhijistudio.transfile.action.FILE_SENDER";
     public final static int FILE_SENDER_START = 1;
